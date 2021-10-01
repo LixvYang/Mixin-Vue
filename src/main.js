0dc2f8c7f1c0d6fc2124398af157bd37cd8b123e
lixin yang <2690688423@qq.com>
2021-10-01 18:50:54 +0800

    complete homemain

--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router/router'
-import store from "./store";
+import store from "./store"
+import { Dropdown,Menu,Icon } from "ant-design-vue";
+// import Atdn from "ant-design-vue"
 
 Vue.config.productionTip = false
+// Vue.use(Atdn)
+Vue.use(Dropdown)
+Vue.use(Menu)
+Vue.use(Icon)
 
 new Vue({
   router,
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,13 +1,30 @@
 <template>
     <div>
-        <h1>Helllllo</h1>
+        <HomeMain></HomeMain>
+        <HomeNet></HomeNet>
+        <HomeMessage></HomeMessage>
+        <HomeAbout></HomeAbout>
     </div>
 </template>
 
-<script>
-
+<script>    
+    import HomeAbout from "./Home/HomeAbout";
+    import HomeMain from "./Home/HomeMain";
+    import HomeMessage from "./Home/HomeMessage";
+    import HomeNet from "./Home/HomeNet";
+    export default {
+        components: {
+            HomeAbout,
+            HomeMain,
+            HomeMessage,
+            HomeNet
+        }
+    }
 </script>
 
 <style>
-
+    html,body{
+        margin: 0;
+        padding: 0;
+    }
 </style>
